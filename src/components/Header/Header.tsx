024f532b0fdce72c705c75baf764b1c3872cb301
thisyujeong <this.yujeong@gmail.com>
2025-01-24 22:10:05 +0900

    feat: 헤더 컴포넌트 작업

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,17 +6,59 @@ import Link from 'next/link';
 
 const cx = classNames.bind(styles);
 
+const sites = [
+  { id: 'github', label: 'github', path: 'https://github.com/thisyujeong' },
+  { id: 'blog', label: 'blog', path: 'https://thisyujeong.dev' },
+];
+
 const Header = () => {
   return (
     <header className={cx('header')}>
+      <div className={cx('header-emblem')}>*</div>
       <div className={cx('header-inner')}>
         <div className={cx('header-bl')}>
           <h1 className={cx('logo')}>
-            <Link href="/">T&nbsp;&nbsp;Y</Link>
+            <Link href="/">
+              <span>T</span>
+              <span>Y</span>
+            </Link>
           </h1>
-          <div className={cx('desc')}>Frontend UX Developer</div>
+          <div className={cx('desc')}>
+            Frontend UX Engineer
+            <br />
+            ©thisyujeong
+          </div>
         </div>
-        <button>INDEX</button>
+
+        <div className={cx('menu-bar')}>
+          <ul className={cx('menu-bar_list')}>
+            <li className={cx('menu-bar_item')}>
+              <Link href="#;">Who I Am</Link>
+            </li>
+            <li className={cx('menu-bar_item')}>
+              <Link href="#;">Experience</Link>
+            </li>
+          </ul>
+
+          <div className={cx('dropdown')}>
+            <button>
+              <span>visit</span>
+            </button>
+            <div className={cx('dropdown_popover')}>
+              <div className={cx('dropdown_popover-inner')}>
+                <ul>
+                  {sites.map(({ id, label, path }) => (
+                    <li>
+                      <Link href={path} target="_blank">
+                        {label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
